1e72e423d9389373c3a28f91e4dd610d453a99e5
Tricky71 <nowwhat@mail.ru>
2023-10-17 20:00:51 +0400

    fin

--- js/main.js
+++ js/main.js
@@ -55,22 +55,85 @@ document.addEventListener('DOMContentLoaded', function() {
 
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 5,
       }
 
     }, {
 
       breakpoint: 600,
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 3,
       }
 
     }]
   })
 
+  $(".slider__list--4").slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    infinite: true, 
+    responsive: [{
 
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 8,
+      }
 
+    }, {
 
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+      }
+
+    }]
+  })
+
+  $(".slider__list--5").slick({
+    slidesToShow: 14,
+    slidesToScroll: 1,
+    autoplay: true,
+    infinite: true, 
+    responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 14,
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 4,
+      }
+
+    }]
+  })
+
+  $(".slider__list--6").slick({
+    slidesToShow: 14,
+    slidesToScroll: 1,
+    autoplay: true,
+    infinite: true, 
+    rtl: true,
+    responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 14,
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 4,
+      }
+
+    }]
+  })
 
 })
 
